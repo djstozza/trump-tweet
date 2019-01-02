6649cb92e251c7cbc8e31603c1c622ae75476b7b
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2019-01-02 19:26:26 +1100

    Minor refactoring of regex matcher

--- app/services/tweet_generator.rb
+++ app/services/tweet_generator.rb
@@ -18,23 +18,23 @@ class TweetGenerator < ApplicationInteraction
 
   def substitute_hash
     {
-      "barack obama" => /\bbar(r){0,1}ack{0,1}\s{0,1}(obama){0,1}/i,
-      "crooked hillary" => /\bhillary{0,1}(\s{0,1}(clinton)){0,1}/i,
-      "lyin' ted" => /\bted{0,1}(\s{0,1}(cruz)){0,1}/i,
+      "barack obama" => /\bbar(r){0,1}ack{0,1}[\s]{0,1}(obama){0,1}/i,
+      "crooked hillary" => /\bhillary{0,1}([\s]{0,1}(clinton)){0,1}/i,
+      "lyin' ted" => /\bted([\s]{0,1}(cruz)){0,1}|cruz/i,
       "cnn" => /cnn/i,
       "obamacare" => /obama/i,
       "lord sugar" => /\b(lord{0,1}(\s|\_){0,1}){0,1}sugar|lord/i,
-      "low energy jeb" => /\bjeb{0,1}(\s{0,1}(bush)){0,1}/i,
-      "lightweight marco" => /\bmarco{0,1}(\s{0,1}rubio){0,1}/i,
+      "low energy jeb" => /\bjeb{0,1}([\s]{0,1}(bush)){0,1}/i,
+      "lightweight marco" => /\bmarco{0,1}([\s]{0,1}rubio){0,1}/i,
       "weak on illegal immigration" => %r{
-        \bted{0,1}(\s{0,1}(cruz)){0,1}|
+        \bted{0,1}([\s]{0,1}(cruz)){0,1}|
         \bmarco{0,1}([\s]{0,1}(rub){0,1}io){0,1}|
-        \bjeb{0,1}(\s{0,1}(bush)){0,1}|
-        \bhillary{0,1}(\s{0,1}(clinton)){0,1}|
+        \bjeb{0,1}([\s]{0,1}(bush)){0,1}|
+        \bhillary{0,1}([\s]{0,1}(clinton)){0,1}|
         \bben([\s]{0,1}carson){0,1}|
         rubio|
         kasich|
-        jo(h){0,1}n ossoff|
+        jo(h){0,1}n\sossoff|
         (doug[\s]{0,1}){0,1}jones
       }ix
     }
